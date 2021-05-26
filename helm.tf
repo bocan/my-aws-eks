867d1d9df73a24853531eec5fec064ba621bd435
Chris Funderburg <chris@cloudcauldron.io>
2021-05-27 00:41:40 +0100

    feat: add aws-load-balancer-controller ingress controller

--- helm.tf
+++ helm.tf
@@ -41,7 +41,32 @@ resource "helm_release" "autoscaler" {
   chart      = "cluster-autoscaler"
 
   values = [
-    templatefile("${path.module}/templates/cluster-autoscaler-chart-values.yaml.tpl", { region = local.region, svc_account = local.k8s_service_account_name, cluster_name = local.cluster_name, role_arn = module.iam_assumable_role_admin.iam_role_arn })
+    templatefile(
+      "${path.module}/templates/cluster-autoscaler-chart-values.yaml.tpl",
+      { region       = local.region,
+        svc_account  = local.k8s_service_account_name,
+        cluster_name = local.cluster_name,
+        role_arn     = module.iam_assumable_role_admin.iam_role_arn
+      }
+    )
   ]
 
+}
+
+
+module "alb_controller" {
+  source = "git::github.com/GSA/terraform-kubernetes-aws-load-balancer-controller?ref=v4.1.0"
+
+  k8s_cluster_type = "eks"
+  k8s_namespace    = "kube-system"
+  aws_tags         = local.tags
+
+  aws_region_name  = local.region
+  k8s_cluster_name = local.cluster_name
+  depends_on = [
+    module.eks, null_resource.kube_config, null_resource.install_calico_plugin
+  ]
+  alb_controller_depends_on = [
+    module.eks
+  ]
 }
