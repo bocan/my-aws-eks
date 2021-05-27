50128b5ef7676c19549695f770b62769d6e55ab1
Chris Funderburg <chris@cloudcauldron.io>
2021-05-27 01:14:49 +0100

    Updated CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,14 @@ project adheres to [Semantic Versioning](http://semver.org/).
 
 <a name="unreleased"></a>
 ## [Unreleased]
+
+
+
+<a name="v0.10.0"></a>
+## [v0.10.0] - 2021-05-27
 FEATURES:
+- Add aws-load-balancer-controller ingress controller
+- Adding changelog generation, version tools, and a Makefile for releases
 - Add changelog generation tooling
 
 
@@ -19,4 +26,5 @@ FEATURES:
 - Add pre-commit tooling, and update one tf module
 
 
-[Unreleased]: https://github.com/bocan/my-aws-eks/compare/v0.9.0...HEAD
+[Unreleased]: https://github.com/bocan/my-aws-eks/compare/v0.10.0...HEAD
+[v0.10.0]: https://github.com/bocan/my-aws-eks/compare/v0.9.0...v0.10.0
--- local.tf
+++ local.tf
@@ -1,7 +1,7 @@
 locals {
   region          = "eu-west-2"
-  vpc_name        = "chrisfu-labs"
-  cluster_name    = "chrisfu-labs-eks-cluster"
+  vpc_name        = "fu-labs"
+  cluster_name    = "fu-labs-eks-cluster"
   cidr            = "172.22.0.0/16"
   cluster_version = "1.20"
   tags = {
@@ -17,20 +17,20 @@ locals {
   on_demand_asg_max_size     = 1
 
   # Spot Pool
-  spot_instance_type    = "t3.medium"
+  spot_instance_type    = "t3.small"
   spot_root_volume_size = 20
   spot_asg_max_size     = 5
   spot_price            = "0.0472"
 
-  #map_accounts = ["123456789012"]
-  map_accounts = []
+  #map_accounts = []
+  map_accounts = ["894121584238"]
 
   # Additional IAM roles to add to the aws-auth configmap.
-  #map_roles    = [{"rolearn": "arn:aws:iam::123456789012:role/MyRole", "username": "role1", "groups": ["system:masters"]}]
-  map_roles = []
+  #map_roles = []
+  map_roles = [{ "rolearn" : "arn:aws:iam::894121584238:role/google-aws-role", "username" : "google-role", "groups" : ["system:masters"] }]
 
   # Additional IAM users to add to the aws-auth configmap.
-  #map_users = [{ "userarn" : "arn:aws:iam::123456789012:user/chris.funderburg@wherever.com", "username" : "chrisfu", "groups" : ["system:masters"] }]
-  map_users = []
+  #map_users    = []
+  map_users = [{ "userarn" : "arn:aws:iam::894121584238:user/chris", "username" : "chris", "groups" : ["system:masters"] }]
 }
 
